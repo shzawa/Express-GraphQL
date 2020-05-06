1bf17d9c1b90e8622fcccff31954ccbd8e70711b
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-05-06 10:14:12 +0900

    update: スプレッド構文を使って視認性に向上

--- schema/mutation/index.js
+++ schema/mutation/index.js
@@ -2,31 +2,14 @@ const {
   GraphQLObjectType,
 } = require('graphql')
 
-const {
-  addMovie,
-  updateMovie,
-  deleteMovie
-} = require('./movie')
-
-const {
-  addDirector,
-  updateDirector,
-  deleteDirector
-} = require('./director')
-
+const Movie = require('./movie')
+const Director = require('./director')
 
 // データ変更方法
 module.exports = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    // movie
-    addMovie,
-    updateMovie,
-    deleteMovie,
-
-    // director
-    addDirector,
-    updateDirector,
-    deleteDirector
+    ...Movie,
+    ...Director
   }
 })
