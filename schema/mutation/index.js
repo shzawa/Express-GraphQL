c1c8630a73512be00c3048636f57890e9c2a349e
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-05-06 10:17:01 +0900

    chore: モジュールを直接スプレッドするようにした

--- schema/mutation/index.js
+++ schema/mutation/index.js
@@ -2,14 +2,11 @@ const {
   GraphQLObjectType,
 } = require('graphql')
 
-const Movie = require('./movie')
-const Director = require('./director')
-
 // データ変更方法
 module.exports = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    ...Movie,
-    ...Director
+    ...require('./movie'),
+    ...require('./director')
   }
 })
