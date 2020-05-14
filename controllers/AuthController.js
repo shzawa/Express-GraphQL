2e1ea555c843dfdf94563a123962c48ef298d805
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-05-14 13:39:57 +0900

    add: Auth#loginにログイン機能実装

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,24 +1,55 @@
-const {
-  graphql
-} = require('graphql')
-const schema = require('../schema')
+const User = require('../schema/models/user')
+const bcrypt = require('bcrypt')
+const func = require('../schema/mutation/func')
 
-exports.test = (req, res) => {
-  let ret = []
-  const query = `
-  {
-    movies {
-      id name
-    }
-  }
-  `
+exports.store = (req, res) => {}
 
-  graphql(schema, query)
-    .then(response => {
-      ret = response.data.movies[0]
-      console.log(ret.name)
-      res.json({
-        name: ret.name
+exports.login = async (req, res) => {
+  // バリデーションチェック
+  if (!req.body.email) return res.status(400).send({
+    message: 'Must be valid email.'
+  })
+  if (!req.body.password) return res.status(400).send({
+    message: 'Must be valid password.'
+  })
+
+  // emailでUser検索
+  await User.exists({
+      email: req.body.email
+    })
+    .then(isExists => {
+      if (!isExists) return res.status(401).send({
+        message: 'Invalid email.'
       })
     })
+
+  const foundUser = await User.findOne({
+      email: req.body.email
+    })
+    .lean()
+    .exec()
+
+  // ログインパスワードチェック
+  await bcrypt.compare(req.body.password, foundUser.password)
+    .then(isLogin => {
+      if (!isLogin) return res.status(401).send({
+        message: 'Login failed.'
+      })
+    })
+
+  // ログイン成功
+  const updateUser = {
+    ...foundUser,
+    hash_id: func.getUniqueStr() // サロゲートキーのみ更新
+  }
+
+  await User.updateOne({
+    _id: updateUser._id
+  }, updateUser)
+
+  return res.status(200)
+    .header({
+      uid: updateUser.hash_id
+    })
+    .send(updateUser)
 }
