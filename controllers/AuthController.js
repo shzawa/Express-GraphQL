bed469d434d6aba5313c804076ed7456bdb55b72
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-05-14 14:06:09 +0900

    add: auth#storeにアカウント登録機能実装

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,7 +2,49 @@ const User = require('../schema/models/user')
 const bcrypt = require('bcrypt')
 const func = require('../schema/mutation/func')
 
-exports.store = (req, res) => {}
+exports.store = async (req, res) => {
+  // バリデーションチェック
+  if (!req.body.name) return res.status(400).send({
+    message: 'Name is required.'
+  })
+  if (!req.body.email) return res.status(400).send({
+    message: 'Email is required.'
+  })
+  if (!req.body.password) return res.status(400).send({
+    message: 'Password is required.'
+  })
+
+  // emailとnameの重複チェック
+  await User.exists({
+      $or: [{
+          email: req.body.email
+        },
+        {
+          name: req.body.name
+        }
+      ]
+    })
+    .then(isExists => {
+      if (isExists) return res.status(400).send({
+        message: 'That name or email has been used.'
+      })
+    })
+
+  const user = await new User({
+    hash_id: func.getUniqueStr(),
+    name: req.body.name,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 10)
+  })
+
+  await user.save()
+
+  return res.status(200)
+    .header({
+      uid: user.hash_id
+    })
+    .send(user)
+}
 
 exports.login = async (req, res) => {
   // バリデーションチェック
